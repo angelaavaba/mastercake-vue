d87814e6130c5575994a2c2f8dbbed6a381e1036
Angela <angelaavalos2410@gmail.com>
2023-11-14 18:59:08 -0600

    Se agregan cambios principales

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="6">
+          <b-card title="Login">
+          <b-form @submit="login">
+
+
+            <b-form-group label="Username">
+              <b-form-input v-model="username" placeholder="Enter your username" required></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Password">
+              <b-form-input v-model="pass" type="password" placeholder="Enter your password" required></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Login</b-button>
+          </b-form>
+        </b-card>
+
+        </b-col>
+        <b-col></b-col>
+
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import console from 'console';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      username: "",
+      pass: ""
+    }
+  },
+  methods: {
+    async login() {
+      const requestBody = {
+        usrn: this.username,
+        password: this.pass
+      }
+
+      const serveUrl = "https://kind-lime-meerkat-gear.cyclic.app/";
+
+      const response = await axios.post(
+        `${serveUrl}users/login`,
+        requestBody
+      );
+      console.log(response);
+    }
   }
 }
 </script>
