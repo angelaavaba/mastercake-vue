f32e46690338703ff04afecaa0f854f534f5a5b5
Angela <angelaavalos2410@gmail.com>
2023-11-16 19:57:10 -0600

    Se agrega slidebar

---
+++ src/views/CategoriasView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    
+     Vista de Categoria
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      username: "",
+      pass: ""
+    }
+  },
+  methods: {
+    async login(event) {
+      event.preventDefault();
+      const requestBody = {
+        usrn: this.username,
+        password: this.pass
+      }
+
+      const serveUrl = "https://kind-lime-meerkat-gear.cyclic.app/";
+
+      const response = await axios.post(
+        `${serveUrl}users/login`,
+        requestBody
+      );
+
+      console.log(response)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-sidebar visible id="sidebar-1" title="Sidebar" shadow>
+     Ventanas de catalogo
+     <router-link to="dashboard/categorias">Catalogo de Categorias</router-link>
+    </b-sidebar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "App",
+    data() {
+        return {
+            username: "",
+            pass: ""
+        };
+    },
+    methods: {
+        async login(event) {
+            event.preventDefault();
+            const requestBody = {
+                usrn: this.username,
+                password: this.pass
+            };
+            const serveUrl = "https://kind-lime-meerkat-gear.cyclic.app/";
+            const response = await axios.post(`${serveUrl}users/login`, requestBody);
+            console.log(response);
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
