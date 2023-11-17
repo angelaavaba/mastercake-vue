f39370d73ee2d2a8f8458f3d00beec57437c78a6
Angela <angelaavalos2410@gmail.com>
2023-11-16 19:30:20 -0600

    Se agragan vistas

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,16 @@
 <template>
   <div id="app">
-    <b-container>
-      <b-row>
-        <b-col></b-col>
-        <b-col cols="6">
-          <b-card title="Register">
-          <b-form @submit="register">
-
-
-            <b-form-group label="Username">
-              <b-form-input v-model="username" placeholder="Enter your username" required></b-form-input>
-            </b-form-group>
-
-            <b-form-group label="Password">
-              <b-form-input v-model="pass" type="password" placeholder="Enter your password" required></b-form-input>
-            </b-form-group>
-
-            <b-form-group label="Birthdate">
-              <b-form-input v-model="birthdate" type="date" placeholder="Enter your birthdate"></b-form-input>
-            </b-form-group>
-
-            <b-form-group label="Address">
-              <b-form-input v-model="address" placeholder="Enter your address"></b-form-input>
-            </b-form-group>
-            
-            <b-form-group label="Gender">
-              <b-form-input v-model="gender" placeholder="Enter your gender (Masculino, Femenino, Other)"></b-form-input>
-            </b-form-group>
-
-
-            <b-button type="submit" variant="primary">Register</b-button>
-          </b-form>
-        </b-card>
-
-        </b-col>
-        <b-col></b-col>
-
-      </b-row>
-    </b-container>
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+
+
 
 export default {
-  name:'App',
-  components: {
-
-  },
-  data(){
-    return{
-      username: "",
-      pass: "",
-      birthdate: "",
-      address: "",
-      gender: ""
-    }
-  },
-  methods:{
-    async register(event){
-      event.preventDefault();
-      const requestBody = {
-        usrn: this.username,
-        password: this.pass,
-        fechaNacimiento: this.birthdate,
-        domicilio: this.address,
-        sexo: this.gender
-      }
-
-      const serveUrl = "https://kind-lime-meerkat-gear.cyclic.app/";
-
-      const response = await axios.post(
-        `${serveUrl}users/registrar`,
-        requestBody
-      );
-      console.log(response)
-    }
-  }
+    name: "App",
+    
 }
 </script>
 
---
+++ src/views/AppLogin.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="6">
+          <b-card title="Login">
+          <b-form @submit="login">
+
+
+            <b-form-group label="Username">
+              <b-form-input v-model="username" placeholder="Enter your username" required></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Password">
+              <b-form-input v-model="pass" type="password" placeholder="Enter your password" required></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Login</b-button>
+          </b-form>
+          <router-link to = "register">Registro</router-link>
+        </b-card>
+
+        </b-col>
+        <b-col></b-col>
+
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      username: "",
+      pass: ""
+    }
+  },
+  methods: {
+    async login(event) {
+      event.preventDefault();
+      const requestBody = {
+        usrn: this.username,
+        password: this.pass
+      }
+
+      const serveUrl = "https://kind-lime-meerkat-gear.cyclic.app/";
+
+      const response = await axios.post(
+        `${serveUrl}users/login`,
+        requestBody
+      );
+
+      console.log(response)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
